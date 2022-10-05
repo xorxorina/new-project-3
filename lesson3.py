011a883ce550e3bed965099a8a51ced787a2ca1d
Ekaterina Khorkhorina <xorxorinaes@mail.ru>
2022-10-05 14:30:56 +0300

    initial

---
+++ lesson3.py
@@ -0,0 +1,23 @@
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+x_diff = x1 - x2
+y_diff = y1 - y2
+if x_diff == 0 == y_diff:
+    print('Ошибка ввода. Введите две разные точки.')
+elif x_diff == 0:
+    print("Уравнение прямой, проходящей через эти точки:")
+    print("x = ", x1)
+elif y_diff == 0:
+    print("Уравнение прямой, проходящей через эти точки:")
+    print("y = ", y1)
+else:
+    k = y_diff / x_diff
+    b = y2 - k * x2
+    print("Уравнение прямой, проходящей через эти точки:")
+    print("y = ", k, " * x + ", b)
+
