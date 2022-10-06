1e70861d7b4ff3b324603c260a0deca328eaa02f
Ekaterina Khorkhorina <xorxorinaes@mail.ru>
2022-10-06 11:19:54 +0300

    VS Code first commit

--- lesson1.py
+++ lesson1.py
@@ -2,5 +2,4 @@ for a in range(1, 10):
 	for b in range(1, 10):
 		print(f'{a} * {b} = {a * b}')
 	print()
-print('Тестовая строка для работы в пичарм')
-print('Тестовая строка to`')
+print('вижуал студио код')
